804b89811edf12adc6caa3e63bbf122673bf18ec
DavidKim0715 <sunkit0526@gmail.com>
2022-02-24 17:07:30 +0900

    Initialized android project

---
+++ app/src/main/assets/www/main.8e7b3f99039f17d7fdb2.js
@@ -0,0 +1,8 @@
+"use strict";
+(self["webpackChunkfrontend_kakaopay"] = self["webpackChunkfrontend_kakaopay"] || []).push([[179],{},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, [736], () => (__webpack_exec__(419)));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
